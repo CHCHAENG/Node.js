1b2833852a347dc5df64621171bf14bea0eed84f
Chaeyoung <oco6029@naver.com>
2022-08-02 16:57:03 +0900

    form 생성, 입력값 받기

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
  
 function templateHTML(title, list, body) {
     return `<!doctype html>
@@ -12,6 +13,7 @@ function templateHTML(title, list, body) {
     <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
     </body>
     </html>
@@ -37,7 +39,6 @@ var app = http.createServer(function(request,response){
 
     if(pathName === '/') {
         if(queryData.id === undefined){
-            
             fs.readdir('data', function(error, filelist){
 
             var title = 'Welcome';
@@ -50,7 +51,6 @@ var app = http.createServer(function(request,response){
             response.writeHead(200);
             response.end(template);
             })
-
         } else {
 
             fs.readdir('data', function(error, filelist){
@@ -65,7 +65,47 @@ var app = http.createServer(function(request,response){
             });
         });
     }
-    } else {
+    } else if(pathName === '/create'){
+        fs.readdir('data', function(error, filelist){
+
+            var title = 'WEB - create';
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `
+            <form action = "http://localhost:3000/create_process" method="post">
+            <p><input type="text" name ="title" placeholder="title"></p>
+            <p>
+                <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
+            `);
+               
+            response.writeHead(200);
+            response.end(template);
+            });
+    } else if(pathName === '/create_process'){
+        var body = '';
+        // web browser로부터 post 방식으로 전송된 데이터를 가져옴
+        request.on('data', function(data){
+            body += data;
+        });
+        // querystring module qs를 이용해서 정보를 객체화 할 수 있음
+        request.on('end',function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+        
+            fs.writeFile(`data/${title}`, description, 'utf8', 
+            function(err){
+                // 리다이렉션
+                response.writeHead(302, {Location: `/?id=${title}`});
+                response.end();
+            })
+        });
+    } 
+    else {
         response.writeHead(404);
         response.end('Not Found');
     }
